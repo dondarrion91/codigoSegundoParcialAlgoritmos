2821ea1470951997611ae8f82902ae97c8e4d2ad
julian <you@example.com>
2020-06-11 12:15:25 -0300

    subida de codigo funcional

--- cmake-build-debug/CMakeFiles/segundoparcial.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/segundoparcial.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/julian/CLionProjects/segundoparcial/main.cpp" "/home/julian/CLionProjects/segundoparcial/cmake-build-debug/CMakeFiles/parcial2.dir/main.cpp.o"
+  "/home/julian/CLionProjects/segundoparcial/main.cpp" "/home/julian/CLionProjects/segundoparcial/cmake-build-debug/CMakeFiles/segundoparcial.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
---
+++ cmake-build-debug/CMakeFiles/segundoparcial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/segundoparcial.dir/main.cpp.o"
+  "segundoparcial"
+  "segundoparcial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/segundoparcial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
